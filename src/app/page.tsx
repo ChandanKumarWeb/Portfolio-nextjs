d2bbef6687ac41c90a45fd2ef5187b3dcbdf7199
ChandanKumarWeb <chandan42kumar55@gmail.com>
2025-06-19 00:00:01 +0530

    added navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 "use- client";
+import Navbar from "./Components/Navbar";
 export default function Home() {
   return (
     <div>
+      <Navbar/>
       <h1 className="bg-black text-gray-300">its Chandan</h1>
     </div>
   );
