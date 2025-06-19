32e6980ea3f40461e2a527e679026546145b75cb
ChandanKumarWeb <chandan42kumar55@gmail.com>
2025-06-19 23:59:54 +0530

    adding hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 "use- client";
 import Navbar from "./Components/Navbar";
+import HeroSection from "./Components/HeroSection";
 export default function Home() {
   return (
     <div>
       <Navbar/>
-      <h1 className="bg-black text-gray-300">its Chandan</h1>
+      <HeroSection />
     </div>
   );
 }
